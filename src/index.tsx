354eff236b3c2cc69d4b941ddafbef63cc21d8d6
Luan-Fones <luanfones@gmail.com>
2024-12-02 20:01:42 -0300

    Estrutura inicial do projeto e configurar do eslint

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const teste = null;
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
