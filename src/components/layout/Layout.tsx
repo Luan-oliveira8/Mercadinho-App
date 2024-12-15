6bc12b5db3d2a04ec69ec2995b1104c97c5ca751
Luan-Fones <luanfones@gmail.com>
2024-12-15 16:47:53 -0300

    Refatorando css da sidebar, porem ainda sem fazer o conteudo do layout ser respeitado

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../header/Header";
-import { LayoutProps } from "./LayoutProps";
 import Sidebar from "../sidebar/Sidebar";
+import { LayoutProps } from "./LayoutProps";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
